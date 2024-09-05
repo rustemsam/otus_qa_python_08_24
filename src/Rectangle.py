e40cb9a8d6cc155ce167a2aecf8378d8bd44f8a3
rustem.samigullin <rustem.samigullin@leaseplan.com>
2024-09-05 12:50:41 +0200

    otus-oop-lesson: add figure,circle, rectangle, square, triangle

---
+++ src/Rectangle.py
@@ -0,0 +1,35 @@
+from src.Figure import Figure
+
+
+class Rectangle(Figure):
+    """
+    Initializes a Rectangle with given side lengths.
+
+    :param a_side: The length of side 'a' of the rectangle.
+    :param b_side: The length of side 'b' of the rectangle.
+    :raises ValueError: If either side length is not positive.
+    """
+
+    def __init__(self, a_side: int, b_side: int):
+        if a_side <= 0 or b_side <= 0:
+            raise ValueError("The side for the side of rectangle should be positive")
+        self.a_side = a_side
+        self.b_side = b_side
+
+    @property
+    def perimeter(self) -> float:
+        """
+        Calculates and returns the perimeter of the rectangle.
+
+        :return: The perimeter of the rectangle.
+        """
+        return (self.a_side + self.b_side) * 2
+
+    @property
+    def area(self) -> float:
+        """
+        Calculates and returns the area of the rectangle.
+
+        :return: The area of the rectangle.
+        """
+        return self.a_side * self.b_side
